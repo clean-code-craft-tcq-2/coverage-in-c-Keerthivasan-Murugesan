7314454ee40914abf4716881da164f6528bf5d3f
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-03-30 22:32:44 +0530

    Initial commit

--- typewise-alert.h
+++ typewise-alert.h
@@ -25,42 +25,8 @@ typedef struct {
   char brand[48];
 } BatteryCharacter;
 
-typedef struct {
-	int lowerLimit;
-	int upperLimit;
-}ValueLimits;
-
-typedef struct {
-	CoolingType coolingType;
-	ValueLimits valueLimits;
-}CoolingTypeLimitPair;
-
-typedef struct {
-	AlertTarget alertTarget;
-	void(*fpSendAlert)(BreachType breachType, void (*fpAlertMethod) (const char*));	
-}AlertTargetAndAlertPair;
-
-typedef struct {
-	BreachType breachType;
-	const char* alertStatement;
-}breachTypeAlertStatementPair;
-
 void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, void (*fpAlertMethod) (const char*));
+  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-bool checkIfValueIsBelowLimit(double value, double limit);
-bool checkIfValueIsAboveLimit(double value, double limit);
-bool checkIfValueIsWithinLimits(double value, double lowerLimit, double upperLimit);
-void checkBreachStatus(bool breachStatus, BreachType breachTypeValidated, BreachType *valueBreachType);
-void inferLowBreach(double value, double lowerLimit, BreachType *valueBreachType);
-void inferHighBreach(double value, double upperLimit, BreachType *valueBreachType);
-void inferNoBreach(double value, double lowerLimit, double upperLimit, BreachType *valueBreachType);
-bool checkIfCoolingTypeMatches(CoolingType coolingType, int index);
-bool checkIfAlertTargetMatches(AlertTarget alertTarget, int index);
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, void (*fpAlertMethod) (const char*));
-void convertBreachTypeToString(BreachType breachType, char* breachTypeString, char* header);
-void sendToController(BreachType breachType, void (*fpAlertMethod) (const char*));
-void sendAlertEmail(int matchingIndex, int index, void (*fpAlertPrint) (const char*), const char* recepient);
-bool checkIfBreachTypeMatches(BreachType breachType, int index);
-void sendToEmail(BreachType breachType, void (*fpAlertMethod) (const char*));
-void print(const char* printStatement);
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
